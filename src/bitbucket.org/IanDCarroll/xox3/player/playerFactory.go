06e4e0199a7a149e1104b6e499afbb938cf267fc
Ian Carroll <ian.d.carroll@gmail.com>
2018-02-11 18:36:07 -0800

    add AiPlayer factory function

--- src/bitbucket.org/IanDCarroll/xox3/player/playerFactory.go
+++ src/bitbucket.org/IanDCarroll/xox3/player/playerFactory.go
@@ -1,17 +1,27 @@
 package player
 
 import (
+  "bitbucket.org/IanDCarroll/xox3/ui/shell"
   "bitbucket.org/IanDCarroll/xox3/ui/display"
   "bitbucket.org/IanDCarroll/xox3/ui/selector"
   "bitbucket.org/IanDCarroll/xox3/ui"
   "bitbucket.org/IanDCarroll/xox3/rules"
   "bitbucket.org/IanDCarroll/xox3/board"
   "bitbucket.org/IanDCarroll/xox3/player/human"
+  "bitbucket.org/IanDCarroll/xox3/player/ai"
 )
 
+var terminalOut shell.ShellOut = shell.NewTerminal()
 
-func BuildHumanMNK3Player(playerNumber int, markers []string, board board.Board) player {
+func BuildHumanPlayer(playerNumber int, markers []string, board board.Board) player {
   ui := human.BuildHumanUi(markers, board)
+  rules := buildMNK3Rules()
+  return New(ui, rules, board, playerNumber)
+}
+
+func BuildAiPlayer(playerNumber int, markers []string, board board.Board) player {
+  spy := human.BuildInGameTerminalDisplay(markers, terminalOut, board)
+  ui := ai.BuildAiUi(board, spy)
   rules := buildMNK3Rules()
   return New(ui, rules, board, playerNumber)
 }
