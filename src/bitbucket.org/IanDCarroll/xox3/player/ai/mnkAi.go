e1e679ed3b22fb751f1e5b02357d34d1080a4771
Ian Carroll <ian.d.carroll@gmail.com>
2018-02-13 15:45:00 -0800

    add minimax that does not lose, but does not win immediately

--- src/bitbucket.org/IanDCarroll/xox3/player/ai/mnkAi.go
+++ src/bitbucket.org/IanDCarroll/xox3/player/ai/mnkAi.go
@@ -26,10 +26,68 @@ func (ai *ai) SetID(id int) {
 }
 
 func (ai *ai) GetMove() int {
-  return ai.dumbButLegalMove()
+  boardCopy := board.Board(ai.publicBoard)
+  initialDepth := 0
+  initialChoices := map[int]int{}
+  return ai.optimumChoice(boardCopy, initialDepth, initialChoices)
 }
 
-func (ai *ai) dumbButLegalMove() int {
-  board := board.Board(ai.publicBoard)
-  return board.AvailableSpaces()[0]
+func (ai *ai) optimumChoice(board board.Board, depth int, choices map[int]int) int {
+  over, report := ai.rules.Gameover(board.View())
+  if over && report == 0 {
+    return report
+  } else if over && report == ai.identity {
+    return 100
+  } else if over && report == ai.opponent {
+    return -1
+  }
+
+  spaces := board.AvailableSpaces()
+  for i := 0; i < len(spaces); i ++ {
+    space := spaces[i]
+    player := ai.appropriatePlayer(board)
+    board.Mark(space, player)
+    choices[space] = ai.optimumChoice(board, depth+1, map[int]int{})
+    board.Mark(space, 0)
+  }
+
+  if depth != 0 {
+    return ai.scoreTheSpace(choices, depth)
+  }
+  return ai.maxSpace(choices)
+}
+
+func (ai *ai) scoreTheSpace(choices map[int]int, depth int) int {
+  if  depth % 2 == 1 { return ai.minScore(choices) }
+  return ai.maxScore(choices)
+}
+
+func (ai *ai) minScore(choices map[int]int) int {
+  min := 100
+  for _, score := range choices {
+    if score < min { min = score }
+  }
+  return min
+}
+
+func (ai *ai) maxScore(choices map[int]int) int {
+  max := -100
+  for _, score := range choices {
+    if score > max { max = score }
+  }
+  return max
+}
+
+func (ai *ai) maxSpace(choices map[int]int) int {
+  bestScore := ai.maxScore(choices)
+  for space, score := range choices {
+    if score == bestScore { return space }
+  }
+  return bestScore
+}
+
+func (ai *ai) appropriatePlayer(board board.Board) int {
+  spacesTaken := board.Size() - len(board.AvailableSpaces())
+  if spacesTaken % 2 == 0 { return 1 }
+  return 2
 }
--- src/bitbucket.org/IanDCarroll/xox3/player/ai/mnkAi_test.go
+++ src/bitbucket.org/IanDCarroll/xox3/player/ai/mnkAi_test.go
@@ -2,34 +2,10 @@ package ai
 
 import (
   "testing"
+  "reflect"
   "bitbucket.org/IanDCarroll/xox3/rules"
   "bitbucket.org/IanDCarroll/xox3/board"
 )
-
-func TestDumbChoiceReturnsZeroFromAnEmptyBoard(t * testing.T) {
-  //Given
-  rules := rules.NewMNKX(3)
-  board := board.NewMNKX(3)
-  subject := NewMNKAi(rules, board)
-  //When
-  actual := subject.dumbButLegalMove()
-  //Then
-  expected := 0
-  assertIntEqual(t, actual, expected)
-}
-
-func TestDumbChoiceReturnsTheFirstAvalilableSpaceFromAPlayedBoard(t * testing.T) {
-  //Given
-  rules := rules.NewMNKX(3)
-  board := board.NewMNKX(3)
-  subject := NewMNKAi(rules, board)
-  board.Mark(0, 1)
-  //When
-  actual := subject.dumbButLegalMove()
-  //Then
-  expected := 1
-  assertIntEqual(t, actual, expected)
-}
 
 func TestAiIdentityDefaultsToPlayingFirst(t * testing.T) {
   //Given
@@ -112,8 +88,517 @@ func TestAiIdentityIsUnaffectedBySetIDCallsWithDefaultValues(t * testing.T) {
   assertIntEqual(t, actualOP, expectedOP)
 }
 
+func TestAppropritatePlayerReturns1IfTheBoardIsEmpty(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  board := board.NewMNKX(3)
+  subject := NewMNKAi(rules, board)
+  //When
+  actual := subject.appropriatePlayer(board)
+  //Then
+  expected := 1
+  assertIntEqual(t, actual, expected)
+}
+
+func TestAppropritatePlayerReturns2IfTheBoardHasBeenMarkedOnce(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  board := board.NewMNKX(3)
+  subject := NewMNKAi(rules, board)
+  board.Mark(0, 1)
+  //When
+  actual := subject.appropriatePlayer(board)
+  //Then
+  expected := 2
+  assertIntEqual(t, actual, expected)
+}
+
+func TestAppropritatePlayerReturns1IfTheBoardHasBeenMarkedTwice(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  board := board.NewMNKX(3)
+  subject := NewMNKAi(rules, board)
+  board.Mark(0, 1)
+  board.Mark(1, 2)
+  //When
+  actual := subject.appropriatePlayer(board)
+  //Then
+  expected := 1
+  assertIntEqual(t, actual, expected)
+}
+
+func TestAppropritatePlayerReturns2IfTheBoardHasBeenMarkedThrice(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  board := board.NewMNKX(3)
+  subject := NewMNKAi(rules, board)
+  board.Mark(0, 1)
+  board.Mark(1, 2)
+  board.Mark(2, 1)
+  //When
+  actual := subject.appropriatePlayer(board)
+  //Then
+  expected := 2
+  assertIntEqual(t, actual, expected)
+}
+
+
+func TestMaxScoreReturnsTheMaxValueFromASlice(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  board := board.NewMNKX(3)
+  subject := NewMNKAi(rules, board)
+  scores := map[int]int{ 1:0, 3:-1, 5:-1, 6:-1, 7:-1, 8:-1 }
+  //When
+  actual := subject.maxScore(scores)
+  //Then
+  expected := 0
+  assertIntEqual(t, actual, expected)
+}
+
+func TestMinScoreReturnsTheMinValueFromASlice(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  board := board.NewMNKX(3)
+  subject := NewMNKAi(rules, board)
+  scores := map[int]int{ 1:0, 3:-1, 5:-1, 6:-1, 7:-1, 8:-1 }
+  //When
+  actual := subject.minScore(scores)
+  //Then
+  expected := -1
+  assertIntEqual(t, actual, expected)
+}
+
+func TestScoreTheSpaceReturnsTheMaxValueIftheDepthIsOdd(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  board := board.NewMNKX(3)
+  subject := NewMNKAi(rules, board)
+  scores := map[int]int{ 1:0, 3:-1, 5:-1, 6:-1, 7:-1, 8:-1 }
+  depth := 2
+  //When
+  actual := subject.scoreTheSpace(scores, depth)
+  //Then
+  expected := 0
+  assertIntEqual(t, actual, expected)
+}
+
+func TestScoreTheSpaceReturnsTheMinValueIftheDepthIsEven(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  board := board.NewMNKX(3)
+  subject := NewMNKAi(rules, board)
+  scores := map[int]int{ 1:0, 3:-1, 5:-1, 6:-1, 7:-1, 8:-1 }
+  depth := 3
+  //When
+  actual := subject.scoreTheSpace(scores, depth)
+  //Then
+  expected := -1
+  assertIntEqual(t, actual, expected)
+}
+
+func TestOptimumChoiceWhenTheBoardIsAlreadyDrawn(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  b := board.NewMNKX(3)
+  subject := NewMNKAi(rules, b)
+  subject.SetID(2)
+  b.Mark(0,2)
+  b.Mark(1,1)
+  b.Mark(2,2)
+  b.Mark(3,2)
+  b.Mark(4,1)
+  b.Mark(5,1)
+  b.Mark(6,1)
+  b.Mark(7,2)
+  b.Mark(8,1)
+  boardCopy := board.Board(b)
+  depth := 0
+  choices := map[int]int{}
+  //When
+  actualChoice := subject.optimumChoice(boardCopy, depth, choices)
+  //Then
+  expectedChoice := 0
+  assertIntEqual(t, actualChoice, expectedChoice)
+}
+
+func TestOptimumChoiceWhenTheBoardIsLostAtTheLastMoveAndAiIsP2(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  b := board.NewMNKX(3)
+  subject := NewMNKAi(rules, b)
+  subject.SetID(2)
+  b.Mark(0,1)
+  b.Mark(1,1)
+  b.Mark(2,1)
+  b.Mark(3,1)
+  b.Mark(4,2)
+  b.Mark(5,2)
+  b.Mark(6,1)
+  b.Mark(7,2)
+  b.Mark(8,2)
+  boardCopy := board.Board(b)
+  depth := 0
+  choices := map[int]int{}
+  //When
+  actualChoice := subject.optimumChoice(boardCopy, depth, choices)
+  //Then
+  expectedChoice := -1
+  assertIntEqual(t, actualChoice, expectedChoice)
+}
+
+func TestOptimumChoiceWhenTheBoardIsWonAtTheLastMoveAndAiIsP1(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  b := board.NewMNKX(3)
+  subject := NewMNKAi(rules, b)
+  subject.SetID(1)
+  b.Mark(0,1)
+  b.Mark(1,1)
+  b.Mark(2,1)
+  b.Mark(3,1)
+  b.Mark(4,2)
+  b.Mark(5,2)
+  b.Mark(6,1)
+  b.Mark(7,2)
+  b.Mark(8,2)
+  boardCopy := board.Board(b)
+  depth := 0
+  choices := map[int]int{}
+  //When
+  actualChoice := subject.optimumChoice(boardCopy, depth, choices)
+  //Then
+  expectedChoice := 100
+  assertIntEqual(t, actualChoice, expectedChoice)
+}
+
+func TestOptimumChoiceWhenTheBoardIsLostEarlyAndAiIsP2(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  b := board.NewMNKX(3)
+  subject := NewMNKAi(rules, b)
+  subject.SetID(2)
+  b.Mark(0,1)
+  b.Mark(1,1)
+  b.Mark(2,1)
+  //3,0
+  //4,0
+  //5,0
+  //6,0
+  b.Mark(7,2)
+  b.Mark(8,2)
+  boardCopy := board.Board(b)
+  depth := 0
+  choices := map[int]int{}
+  //When
+  actualChoice := subject.optimumChoice(boardCopy, depth, choices)
+  //Then
+  expectedChoice := -1
+  assertIntEqual(t, actualChoice, expectedChoice)
+}
+
+func TestOptimumChoiceWhenTheBoardIsWonEarlyAndAiIsP2(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  b := board.NewMNKX(3)
+  subject := NewMNKAi(rules, b)
+  subject.SetID(2)
+  b.Mark(0,1)
+  b.Mark(1,1)
+  //2,0
+  //3,0
+  b.Mark(4,1)
+  //5,0
+  b.Mark(6,2)
+  b.Mark(7,2)
+  b.Mark(8,2)
+  boardCopy := board.Board(b)
+  depth := 0
+  choices := map[int]int{}
+  //When
+  actualChoice := subject.optimumChoice(boardCopy, depth, choices)
+  //Then
+  expectedChoice := 100
+  assertIntEqual(t, actualChoice, expectedChoice)
+}
+
+func TestOptimumChoiceWhenTheBoardWillBeDrawn(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  b := board.NewMNKX(3)
+  subject := NewMNKAi(rules, b)
+  subject.SetID(1)
+  b.Mark(0,2)
+  b.Mark(1,1)
+  b.Mark(2,2)
+  b.Mark(3,2)
+  b.Mark(4,1)
+  b.Mark(5,1)
+  //6,0
+  b.Mark(7,2)
+  b.Mark(8,1)
+  boardCopy := board.Board(b)
+  depth := 0
+  choices := map[int]int{}
+  //When
+  actualChoice := subject.optimumChoice(boardCopy, depth, choices)
+  //Then
+  expectedChoice := 6
+  assertIntEqual(t, actualChoice, expectedChoice)
+}
+
+func TestOptimumChoiceWhenTheBoardWillBeWonAtTheLastMove(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  b := board.NewMNKX(3)
+  subject := NewMNKAi(rules, b)
+  subject.SetID(1)
+  //0,0
+  b.Mark(1,1)
+  b.Mark(2,1)
+  b.Mark(3,1)
+  b.Mark(4,2)
+  b.Mark(5,2)
+  b.Mark(6,1)
+  b.Mark(7,2)
+  b.Mark(8,2)
+  boardCopy := board.Board(b)
+  depth := 0
+  choices := map[int]int{}
+  //When
+  actualChoice := subject.optimumChoice(boardCopy, depth, choices)
+  //Then
+  expectedChoice := 0
+  assertIntEqual(t, actualChoice, expectedChoice)
+}
+
+func TestOptimumChoiceWhenAiIsP2AndThereAre2MovesLeftOneDrawsAndOneLoses(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  b := board.NewMNKX(3)
+  subject := NewMNKAi(rules, b)
+  subject.SetID(2)
+  //0,0
+  b.Mark(1,1)
+  b.Mark(2,2)
+  //3,0
+  b.Mark(4,1)
+  b.Mark(5,1)
+  b.Mark(6,1)
+  b.Mark(7,2)
+  b.Mark(8,2)
+  boardCopy := board.Board(b)
+  depth := 0
+  choices := map[int]int{}
+  //When
+  actualChoice := subject.optimumChoice(boardCopy, depth, choices)
+  //then
+  expectedChoice := 3
+  assertIntEqual(t, actualChoice, expectedChoice)
+}
+
+func TestOptimumChoiceWhenAiIsP2AndThereAre2MovesLeftOneWinsAndOneDraws(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  b := board.NewMNKX(3)
+  subject := NewMNKAi(rules, b)
+  subject.SetID(2)
+  b.Mark(0,2)
+  b.Mark(1,1)
+  b.Mark(2,1)
+  b.Mark(3,1)
+  b.Mark(4,2)
+  b.Mark(5,2)
+  b.Mark(6,1)
+  //7,0
+  //8,0
+  boardCopy := board.Board(b)
+  depth := 0
+  choices := map[int]int{}
+  //When
+  actualChoice := subject.optimumChoice(boardCopy, depth, choices)
+  //Then
+  expectedChoice := 8
+  assertIntEqual(t, actualChoice, expectedChoice)
+}
+
+func TestOptimumChoiceWhenAiIsP2AndThereAre2MovesLeftOneWinsOneLoses(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  b := board.NewMNKX(3)
+  subject := NewMNKAi(rules, b)
+  subject.SetID(2)
+  b.Mark(0,1)
+  b.Mark(1,1)
+  //2,0
+  b.Mark(3,1)
+  b.Mark(4,2)
+  b.Mark(5,1)
+  //6,0
+  b.Mark(7,2)
+  b.Mark(8,2)
+  boardCopy := board.Board(b)
+  depth := 0
+  choices := map[int]int{}
+  //When
+  actualChoice := subject.optimumChoice(boardCopy, depth, choices)
+  //then
+  expectedChoice := 6
+  assertIntEqual(t, actualChoice, expectedChoice)
+}
+
+func TestOptimumChoiceWithTestBoardNumber1(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  b := board.NewMNKX(3)
+  subject := NewMNKAi(rules, b)
+  subject.SetID(1)
+  b.Mark(0,1)
+  b.Mark(1,2)
+  b.Mark(2,1)
+  b.Mark(3,2)
+  b.Mark(4,1)
+  b.Mark(5,2)
+  //6,0
+  b.Mark(7,1)
+  b.Mark(8,2)
+  boardCopy := board.Board(b)
+  depth := 0
+  choices := map[int]int{}
+  //When
+  actualChoice := subject.optimumChoice(boardCopy, depth, choices)
+  //Then
+  expectedChoice := 6
+  assertIntEqual(t, actualChoice, expectedChoice)
+}
+
+func TestOptimumChoiceWithTestBoardNumber2(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  b := board.NewMNKX(3)
+  subject := NewMNKAi(rules, b)
+  subject.SetID(1)
+  b.Mark(0,1)
+  b.Mark(1,2)
+  b.Mark(2,1)
+  b.Mark(3,2)
+  b.Mark(4,1)
+  //5,0
+  //6,0
+  //7,0
+  b.Mark(8,2)
+  boardCopy := board.Board(b)
+  depth := 0
+  choices := map[int]int{}
+  //When
+  actualChoice := subject.optimumChoice(boardCopy, depth, choices)
+  //Then
+  expectedChoice := 6
+  assertIntEqual(t, actualChoice, expectedChoice)
+}
+
+func TestOptimumChoiceWithTestBoardNumber3(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  b := board.NewMNKX(3)
+  subject := NewMNKAi(rules, b)
+  subject.SetID(2)
+  b.Mark(0,1)
+  b.Mark(1,2)
+  b.Mark(2,1)
+  b.Mark(3,2)
+  b.Mark(4,1)
+  b.Mark(5,1)
+  //6,0
+  //7,0
+  b.Mark(8,2)
+  boardCopy := board.Board(b)
+  depth := 0
+  choices := map[int]int{}
+  //When
+  actualChoice := subject.optimumChoice(boardCopy, depth, choices)
+  //Then
+  expectedChoice := 6
+  assertIntEqual(t, actualChoice, expectedChoice)
+}
+
+func TestOptimumChoiceWhenThereIsAnEarlyChanceToToWinOrLoseOnRows(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  b := board.NewMNKX(3)
+  subject := NewMNKAi(rules, b)
+  subject.SetID(2)
+  b.Mark(0,1)
+  //1,0
+  b.Mark(2,1)
+  //3,0
+  b.Mark(4,1)
+  //4,0
+  b.Mark(6,2)
+  //7,0
+  b.Mark(8,2)
+  boardCopy := board.Board(b)
+  depth := 0
+  choices := map[int]int{}
+  //When
+  actualChoice := subject.optimumChoice(boardCopy, depth, choices)
+  //Then
+  expectedChoice := 7
+  assertIntEqual(t, actualChoice, expectedChoice)
+}
+
+func TestOptimumChoiceWhenThereIsAnEarlyChanceToToWinOrLoseOn2(t * testing.T) {
+  //Given
+  rules := rules.NewMNKX(3)
+  b := board.NewMNKX(3)
+  subject := NewMNKAi(rules, b)
+  subject.SetID(2)
+  b.Mark(0,1)
+  b.Mark(1,1)
+  //2,0
+  //3,0
+  //4,0
+  //4,0
+  //5,0
+  //6,0
+  //7,0
+  b.Mark(8,2)
+  boardCopy := board.Board(b)
+  depth := 0
+  choices := map[int]int{}
+  //When
+  actualChoice := subject.optimumChoice(boardCopy, depth, choices)
+  //Then
+  expectedChoice := 2
+  assertIntEqual(t, actualChoice, expectedChoice)
+}
+
 func assertIntEqual(t *testing.T, actual, expected int) {
   if actual != expected {
     t.Errorf("\n%d\nshould have been\n%d", actual, expected)
+  }
+}
+
+func assertChoicesEqual(t *testing.T, actual, expected []map[int]int) {
+  if !reflect.DeepEqual(actual, expected) {
+    t.Errorf("\n%d\nshould be\n%d", actual, expected)
+  }
+}
+
+func assertChoiceEqual(t *testing.T, actual, expected map[int]int) {
+  if !reflect.DeepEqual(actual, expected) {
+    t.Errorf("\n%d\nshould be\n%d", actual, expected)
+  }
+}
+
+func assertSlicesEqual(t *testing.T, actual, expected []int) {
+  if !reflect.DeepEqual(actual, expected) {
+    t.Errorf("\n%d\nshould be\n%d", actual, expected)
+  }
+}
+
+func assertBoardsEqual(t *testing.T, actual, expected [][]int) {
+  if !reflect.DeepEqual(actual, expected) {
+    t.Errorf("\n%d\nshould be\n%d", actual, expected)
   }
 }
